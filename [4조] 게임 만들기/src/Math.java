3db71868738c80be9f3b75b4a63d6755738a5159
0gon <dudrhs4563@naver.com>
2023-06-22 14:39:43 +0900

    나누기

--- [4조] 게임 만들기/src/Math.java
+++ [4조] 게임 만들기/src/Math.java
@@ -2,13 +2,17 @@
 public class Math {
 
 	// 효련 더하기 인트 머시기
-	
-	// 지원 뺴기 
-	
+
+	// 지원 뺴기
+
 	// 강현 곱하기
-	
-	// 주환 나누기 
-	
-	// 냥곤 냐옹냐옹 
-	
+
+	// 주환 나누기
+	public void 나누기() {
+		int a = 1;
+		int b = 3;
+		System.out.println(a / b);
+	}
+	// 냥곤 냐옹냐옹
+
 }
