050baa27eab78ad32c2b3ecfe6bb9966c90bd225
01100101 <9212kang@gmail.com>
2023-06-22 14:40:22 +0900

    빼기. 강지원

--- [4조] 게임 만들기/src/Math.java
+++ [4조] 게임 만들기/src/Math.java
@@ -1,14 +1,14 @@
 
 public class Math {
+	public static void main(String[] args) {
+		// 효련 더하기 인트값 2개 합 반환
 
-	// 효련 더하기 인트 머시기
-	
-	// 지원 뺴기 
-	
-	// 강현 곱하기
-	
-	// 주환 나누기 
-	
-	// 냥곤 냐옹냐옹 
-	
+		// 지원 빼기
+		System.out.println("빼기");
+		// 강현 곱하기
+
+		// 주환 나누기
+
+		// 냥곤 냐옹냐옹
+	}
 }
