9869e10f4b67c6a7979986f067157450954620e5
01100101 <9212kang@gmail.com>
2023-06-23 18:07:42 +0900

    Gui 다시 밈

--- [4조] 게임 만들기/src/GUI/RankWin.java
+++ [4조] 게임 만들기/src/GUI/RankWin.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class RankWin extends JFrame {
 
@@ -35,14 +40,151 @@ public class RankWin extends JFrame {
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
-		// 공룡게임 프레임 설정
-		setTitle("인벤토리"); // 타이틀 이름
+		//  프레임 설정
+		setTitle("랭킹확인"); // 타이틀 이름
 		setResizable(false); // 창의 크기를 변경하지 못하게
 		setLocationRelativeTo(null); // 창이 가운데 나오게
-		getContentPane().setLayout(null); // 레이아웃을 내맘대로 설정가능하게 해줌.
+		getContentPane().setLayout(null);
+		
+		JButton Backbtn = new JButton();
+		
+		JPanel Schoolpanel = new JPanel();
+		
+		JPanel Studentpanel = new JPanel();
+		
+		JPanel AllStudentpanel = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addComponent(Schoolpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(Studentpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(AllStudentpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(24, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(734, Short.MAX_VALUE)
+					.addComponent(Backbtn, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(Backbtn, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addGap(111)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(Schoolpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+						.addComponent(Studentpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+						.addComponent(AllStudentpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(150, Short.MAX_VALUE))
+		);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		
+		JLabel lblNewLabel_4_2 = new JLabel("New label");
+		
+		JLabel lblNewLabel_5_2 = new JLabel("New label");
+		
+		JLabel lblNewLabel_3_2 = new JLabel("New label");
+		GroupLayout gl_AllStudentpanel = new GroupLayout(AllStudentpanel);
+		gl_AllStudentpanel.setHorizontalGroup(
+			gl_AllStudentpanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_AllStudentpanel.createSequentialGroup()
+					.addContainerGap(101, Short.MAX_VALUE)
+					.addGroup(gl_AllStudentpanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_3_2)
+						.addComponent(lblNewLabel_4_2)
+						.addComponent(lblNewLabel_5_2)
+						.addComponent(lblNewLabel_2))
+					.addGap(92))
+		);
+		gl_AllStudentpanel.setVerticalGroup(
+			gl_AllStudentpanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_AllStudentpanel.createSequentialGroup()
+					.addGap(31)
+					.addComponent(lblNewLabel_2)
+					.addGap(36)
+					.addComponent(lblNewLabel_3_2)
+					.addGap(38)
+					.addComponent(lblNewLabel_4_2)
+					.addGap(44)
+					.addComponent(lblNewLabel_5_2)
+					.addContainerGap(41, Short.MAX_VALUE))
+		);
+		AllStudentpanel.setLayout(gl_AllStudentpanel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		
+		JLabel lblNewLabel_4_1 = new JLabel("New label");
+		
+		JLabel lblNewLabel_5_1 = new JLabel("New label");
+		
+		JLabel lblNewLabel_3_1 = new JLabel("New label");
+		GroupLayout gl_Studentpanel = new GroupLayout(Studentpanel);
+		gl_Studentpanel.setHorizontalGroup(
+			gl_Studentpanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_Studentpanel.createSequentialGroup()
+					.addContainerGap(101, Short.MAX_VALUE)
+					.addGroup(gl_Studentpanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_3_1)
+						.addComponent(lblNewLabel_4_1)
+						.addComponent(lblNewLabel_5_1)
+						.addComponent(lblNewLabel_1))
+					.addGap(92))
+		);
+		gl_Studentpanel.setVerticalGroup(
+			gl_Studentpanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_Studentpanel.createSequentialGroup()
+					.addGap(28)
+					.addComponent(lblNewLabel_1)
+					.addGap(37)
+					.addComponent(lblNewLabel_3_1)
+					.addGap(38)
+					.addComponent(lblNewLabel_4_1)
+					.addGap(44)
+					.addComponent(lblNewLabel_5_1)
+					.addContainerGap(43, Short.MAX_VALUE))
+		);
+		Studentpanel.setLayout(gl_Studentpanel);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		GroupLayout gl_Schoolpanel = new GroupLayout(Schoolpanel);
+		gl_Schoolpanel.setHorizontalGroup(
+			gl_Schoolpanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_Schoolpanel.createSequentialGroup()
+					.addGap(96)
+					.addGroup(gl_Schoolpanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_5)
+						.addComponent(lblNewLabel_4)
+						.addComponent(lblNewLabel_3)
+						.addComponent(lblNewLabel))
+					.addContainerGap(97, Short.MAX_VALUE))
+		);
+		gl_Schoolpanel.setVerticalGroup(
+			gl_Schoolpanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_Schoolpanel.createSequentialGroup()
+					.addGap(24)
+					.addComponent(lblNewLabel)
+					.addGap(43)
+					.addComponent(lblNewLabel_3)
+					.addGap(38)
+					.addComponent(lblNewLabel_4)
+					.addPreferredGap(ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+					.addComponent(lblNewLabel_5)
+					.addGap(41))
+		);
+		Schoolpanel.setLayout(gl_Schoolpanel);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
--- [4조] 게임 만들기/src/GUI/StoreWin.java
+++ [4조] 게임 만들기/src/GUI/StoreWin.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class StoreWin extends JFrame {
 
@@ -32,11 +37,59 @@ public class StoreWin extends JFrame {
 	 */
 	public StoreWin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		//  프레임 설정
+		setTitle("상점"); // 타이틀 이름
+		setResizable(false); // 창의 크기를 변경하지 못하게
+		setLocationRelativeTo(null); // 창이 가운데 나오게
+		getContentPane().setLayout(null);
+		
+		JButton Backbtn = new JButton();
+		
+		JLabel Coinlbl = new JLabel("1.000");
+		
+		JPanel Charpnl = new JPanel();
+		
+		JPanel Pickcharpanel = new JPanel();
+		
+		JPanel Pickbackpanel = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(30)
+					.addComponent(Pickcharpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addGap(31)
+					.addComponent(Pickbackpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+					.addGap(35)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(Coinlbl, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+							.addGap(11)
+							.addComponent(Backbtn, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+						.addComponent(Charpnl, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(38, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(162)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(Backbtn, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(17)
+							.addComponent(Coinlbl)
+							.addGap(18)
+							.addComponent(Charpnl, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE))
+						.addComponent(Pickbackpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+						.addComponent(Pickcharpanel, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(149, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
