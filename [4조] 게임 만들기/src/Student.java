01fb6c791d8eb0399ecfc00cb77ba7068346b05d
juhwan <zndntn66@gmail.com>
2023-06-23 16:04:40 +0900

    student 초안

--- [4조] 게임 만들기/src/Student.java
+++ [4조] 게임 만들기/src/Student.java
@@ -1,4 +1,57 @@
 
 public class Student {
-	
+	String id;
+	String password;
+	String school;
+	int point;
+
+	public Student(String id, String password, String school, int point) {
+		this.id = id;
+		this.password = password;
+		this.school = school;
+		this.point = point;
+	}
+
+	public Student(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", password=" + password + ", school=" + school + ", point=" + point + "]";
+	}
+
 }
