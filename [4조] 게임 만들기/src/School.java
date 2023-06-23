011152fc00b5d3862a533e7fcee835a162567d71
juhwan <zndntn66@gmail.com>
2023-06-23 16:09:24 +0900

    school 초안

---
+++ [4조] 게임 만들기/src/School.java
@@ -0,0 +1,32 @@
+
+public class School {
+	String userId;
+	int pointAll;
+
+	public School(String userId, int pointAll) {
+		this.userId = userId;
+		this.pointAll = pointAll;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getPointAll() {
+		return pointAll;
+	}
+
+	public void setPointAll(int pointAll) {
+		this.pointAll = pointAll;
+	}
+
+	@Override
+	public String toString() {
+		return "School [userId=" + userId + ", pointAll=" + pointAll + "]";
+	}
+
+}
