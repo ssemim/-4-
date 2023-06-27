229046f794d93e3c1821b732f1cfe7d1b4e58d36
0gon <dudrhs4563@naver.com>
2023-06-27 14:18:20 +0900

    인벤gui안의 메소드 분리해서 저장

--- [4조] 게임 만들기/src/메소드모음/Inventory.java
+++ [4조] 게임 만들기/src/메소드모음/Inventory.java
@@ -7,6 +7,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import GUI.invenWin;
 import dbutil.DBUtil;
 import 객체모음.Student;
 
@@ -47,5 +52,88 @@ public class Inventory {
 			DBUtil.close(conn);
 		}
 		return l;
+	}
+	
+	public void imageSet(List cList, List cList2, List wList, List wList2, 
+			ImageIcon[] cIcons, ImageIcon[] cIconsBlock, ImageIcon[] wIcons, ImageIcon[] wIconsBlock) {
+		for (int i = 0; i < cList2.size(); i++) {
+			cIcons[i] = new ImageIcon(invenWin.class.getResource("/이미지/캐릭터" + (i + 1) + ".gif"));
+		}
+		for (int i = 0; i < cList2.size(); i++) {
+			cIconsBlock[i] = new ImageIcon(invenWin.class.getResource("/이미지/캐릭터" + (i + 1) + "잠금.png"));
+		}
+
+		for (int i = 0; i < wList2.size(); i++) {
+			wIcons[i] = new ImageIcon(invenWin.class.getResource("/이미지/배경" + (i + 1) + ".png"));
+		}
+		for (int i = 0; i < wList2.size(); i++) {
+			wIconsBlock[i] = new ImageIcon(invenWin.class.getResource("/이미지/배경" + (i + 1) + "잠금.png"));
+		}
+	}
+
+	
+
+	public int cBtnRight(int cCount, JLabel cLbl, JButton cBtnRight, JButton cBtnLeft,
+			List cList, List cList2, ImageIcon[] cIcons, ImageIcon[] cIconsBlock) {
+		
+		if (cCount < cList2.size())
+			cCount += 1;
+		if ((boolean) cList.get(cCount)) {
+			cLbl.setIcon(cIcons[cCount]);
+		} else {
+			cLbl.setIcon(cIconsBlock[cCount]);
+		}
+		if (cCount == cList2.size() - 1)
+			cBtnRight.setEnabled(false);
+		cBtnLeft.setEnabled(true);
+		return cCount;
+		
+	}
+
+	public int cBtnLeft(int cCount, JLabel cLbl, JButton cBtnRight, JButton cBtnLeft,
+			List cList, List cList2, ImageIcon[] cIcons, ImageIcon[] cIconsBlock) {
+		if (cCount > 0)
+			cCount -= 1;
+		if ((boolean) cList.get(cCount)) {
+			cLbl.setIcon(cIcons[cCount]);
+		} else {
+			cLbl.setIcon(cIconsBlock[cCount]);
+		}
+		if (cCount == 0)
+			cBtnLeft.setEnabled(false);
+		cBtnRight.setEnabled(true);
+		return cCount;
+	}
+	
+	public int wBtnLeft(int wCount, JLabel wLbl, JButton wBtnRight, JButton wBtnLeft,
+			List wList, List wList2, ImageIcon[] wIcons, ImageIcon[] wIconsBlock) {
+		if (wCount > 0)
+			wCount -= 1;
+		if ((boolean) wList.get(wCount)) {
+			wLbl.setIcon(wIcons[wCount]);
+		} else {
+			wLbl.setIcon(wIconsBlock[wCount]);
+		}
+		if (wCount == 0)
+			wBtnLeft.setEnabled(false);
+		wBtnRight.setEnabled(true);
+		System.out.println(wCount);
+		return wCount;
+	}
+
+	public int wBtnRight(int wCount, JLabel wLbl, JButton wBtnRight, JButton wBtnLeft,
+			List wList, List wList2, ImageIcon[] wIcons, ImageIcon[] wIconsBlock) {
+		if (wCount < wList2.size())
+			wCount += 1;
+		if ((boolean) wList.get(wCount)) {
+			wLbl.setIcon(wIcons[wCount]);
+		} else {
+			wLbl.setIcon(wIconsBlock[wCount]);
+		}
+		if (wCount == wList2.size() - 1)
+			wBtnRight.setEnabled(false);
+		wBtnLeft.setEnabled(true);
+		System.out.println(wCount);
+		return wCount;
 	}
 }
