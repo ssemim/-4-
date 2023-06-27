99f23c4abeac88802e99c2d069ab1b8fbcf7e23c
01100101 <9212kang@gmail.com>
2023-06-27 10:42:42 +0900

    commit

--- [4조] 게임 만들기/src/GUI/MainWin.java
+++ [4조] 게임 만들기/src/GUI/MainWin.java
@@ -15,6 +15,7 @@ import javax.swing.border.EmptyBorder;
 import 객체모음.Student;
 import javax.swing.ImageIcon;
 import java.awt.Color;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class MainWin extends JFrame {
 
@@ -56,6 +57,7 @@ public class MainWin extends JFrame {
 
 		// 게임하기버튼을 누르면 SelectgameWin으로 이동하는 액션리스너
 		JButton Gamebtn = new JButton(); // 인벤 버튼
+		Gamebtn.setBounds(129, 111, 240, 80);
 		Gamebtn.setIcon(new ImageIcon(MainWin.class.getResource("/이미지/게임하기버튼.png")));
 		Gamebtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -66,6 +68,7 @@ public class MainWin extends JFrame {
 		});
 		
 		JButton invenbtn = new JButton(""); // 인벤 버튼
+		invenbtn.setBounds(129, 222, 240, 80);
 		invenbtn.setIcon(new ImageIcon(MainWin.class.getResource("/이미지/인벤토리버튼.png")));
 		// 인벤토리버튼을 누르면 invenWin으로 이동하는 액션리스너
 		invenbtn.addActionListener(new ActionListener() {
@@ -77,6 +80,7 @@ public class MainWin extends JFrame {
 		});
 		
 		JButton rankbtn = new JButton(""); // 랭킹 버튼
+		rankbtn.setBounds(129, 331, 240, 80);
 		rankbtn.setIcon(new ImageIcon(MainWin.class.getResource("/이미지/랭킹버튼.png")));
 		// 랭킹버튼을 누르면 RankWin으로 이동하는 액션리스너
 		rankbtn.addActionListener(new ActionListener() {
@@ -87,6 +91,7 @@ public class MainWin extends JFrame {
 			}
 		});
 		JButton Storebtn = new JButton(""); // 상점 버튼
+		Storebtn.setBounds(129, 443, 240, 80);
 		Storebtn.setIcon(new ImageIcon(MainWin.class.getResource("/이미지/상점버튼.png")));
 		// 상점버튼을 누르면 StoreWin으로 이동하는 액션리스너
 		Storebtn.addActionListener(new ActionListener() {
@@ -97,39 +102,26 @@ public class MainWin extends JFrame {
 			}
 		});
 		JPanel Charpanel = new JPanel(); // 캐릭터 이미지가 나오는 Panel
-
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
-					.addGroup(gl_contentPane.createSequentialGroup()
-						.addGap(124)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(Storebtn, GroupLayout.PREFERRED_SIZE, 240, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(rankbtn, GroupLayout.PREFERRED_SIZE, 240,
-												GroupLayout.PREFERRED_SIZE)
-										.addComponent(invenbtn, GroupLayout.PREFERRED_SIZE, 240,
-												GroupLayout.PREFERRED_SIZE))
-								.addGap(133)
-								.addComponent(Charpanel, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)) // 캐릭터
-																														// 판넬
-																														// 가로
-						.addComponent(Gamebtn, GroupLayout.PREFERRED_SIZE, 240, GroupLayout.PREFERRED_SIZE))
-				.addContainerGap(137, Short.MAX_VALUE)));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addGroup(gl_contentPane
-				.createSequentialGroup().addContainerGap(77, Short.MAX_VALUE)
-				.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(Charpanel, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE) // 캐릭터 판넬
-																												// 가로
-						.addGroup(gl_contentPane.createSequentialGroup()
-								.addComponent(Gamebtn, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
-								.addGap(31)
-								.addComponent(invenbtn, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
-								.addGap(29)
-								.addComponent(rankbtn, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)))
-				.addGap(32).addComponent(Storebtn, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
-				.addGap(72)));
-		contentPane.setLayout(gl_contentPane);
+		Charpanel.setBounds(502, 211, 150, 200);
+		contentPane.setLayout(null);
+		
+		JButton cutbtn = new JButton(); // 종료버튼
+		cutbtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
+		
+		cutbtn.setLocation(758, 10);
+		cutbtn.setSize(30, 30);
+		contentPane.add(cutbtn);
+		contentPane.add(Storebtn);
+		contentPane.add(rankbtn);
+		contentPane.add(invenbtn);
+		contentPane.add(Charpanel);
+		contentPane.add(Gamebtn);
 	}
 }
